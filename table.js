8373097c349c65331d41a3d5277d65deba99de71
srushtipadval <67824583+srushtipadval@users.noreply.github.com>
2020-07-26 23:49:28 +0530

    Update table.js

--- table.js
+++ table.js
@@ -1,8 +1,8 @@
 const name = document.getElementById('name');
-const email = document.getElementById('Age');
-const phone = document.getElementById('email');
+const Age = document.getElementById('Age');
+const email = document.getElementById('email');
 const password = document.getElementById('password');
-const date = document.getElementById('phone');
+const phone = document.getElementById('phone');
 const form = document.getElementById('form');
 const errorElement = document.getElementById('error');
 form.addEventListener('submit',(e)=>{
